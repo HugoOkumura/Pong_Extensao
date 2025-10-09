7eed1c1794a95c36c4bc35daf94a9c1b074f4fb0
Hugo N. S. Okumura <111467674+HugoOkumura@users.noreply.github.com>
2025-10-09 19:25:04 -0300

    Ajeitando o código
    dividindo o código em funções expecíficas

--- pong.ino
+++ pong.ino
@@ -20,6 +20,101 @@ uint8_t estado;
 uint8_t jogador_vitorioso;
 char leitura;
 
+/************************************************************
+ * 
+ *                  Colisão da Bolinha
+ * 
+*************************************************************/
+void colisao_jogador(uint8_t circle_x, uint8_t circle_y){
+  //batendo no P1 ou na quina
+  if(circle_x == 8 || circle_x == 7){
+  //bateu no meio
+  if(circle_y <= p1_cima-6 && circle_y >= p1_baixo+6){
+    dir_y = 0;
+    dir_x *= -1;
+  }
+  //bateu em cima
+  else if(circle_y <= p1_cima && circle_y >= p1_cima-5){
+    dir_y= 1;
+    dir_x *= -1;
+  }
+  //bateu em baixo
+  else if(circle_y >= p1_baixo && circle_y <= p1_baixo+5){
+    dir_y= -1;
+    dir_x *= -1;
+  }
+  }
+  //batendo no P2 ou na
+  if(circle_x == 115 || circle_x ==114){
+  //bateu no meio
+  if(circle_y <= p2_cima-6 && circle_y >= p2_baixo+6){
+    dir_y = 0;
+    dir_x *= -1;
+  }
+  //bateu em cima
+  else if(circle_y <= p2_cima && circle_y >= p2_cima-5){
+    dir_y= 1;
+    dir_x *= -1;
+  }
+  //bateu em baixo
+  else if(circle_y >= p2_baixo && circle_y <= p2_baixo+5){
+    dir_y= -1;
+    dir_x *= -1;
+  }
+  }
+}
+
+void colisao_campo(uint8_t circle_x, uint8_t circle_y){
+  //colisão em cima
+  if(circle_y == 8){
+  dir_y *= -1;
+  }
+ 
+  //colisão em baixo
+  if(circle_y == 91){
+  dir_y *= -1;
+  }
+}
+
+void colisao_pontuacao(uint8_t circle_x, uint8_t circle_y){
+
+  //Bateu na direita
+  if(circle_x >= 120){
+  dir_x *= -1;
+  dir_y= 0;
+  p1_pontuacao += 1; //Aumenta pontuação do Player 1
+
+  if(p1_pontuacao == 5){
+
+    estado = VITORIA;
+
+    jogador_vitorioso = 1;
+  }
+
+  //Inicializa a posição da bola
+  circle_x = TV.hres()/2;
+  circle_y = TV.vres()/2;
+  }
+  //Bateu na esquerda
+  else if(circle_x <= 2){
+  dir_x *= -1;
+  dir_y= 0;
+  p2_pontuacao += 1; //Aumenta pontuação do Player 2
+
+  if(p2_pontuacao == 5){
+
+    estado = VITORIA;
+
+    jogador_vitorioso = 2;
+  }
+
+  //Inicializa a posição da bola
+  circle_x = TV.hres()/2;
+  circle_y = TV.vres()/2;
+  }
+
+}
+
 void setup() {
 
   // TV.begin(_PAL, 640, 360);
@@ -84,51 +179,26 @@ void menu(){
 }
 
 void jogo(){
-  // TV.select_font(font8x8);
-  // Serial.println(TV.hres());
-  // Serial.printlnS(TV.vres());
-  // TV.fill(WHITE);
   
-  TV.draw_circle(circle_x, circle_y, 2, WHITE, WHITE);
- 
-//  for(int i = 2; i < TV.hres()-4; i+=2){
-//  TV.draw_circle(i, TV.vres()/2, 2, WHITE,WHITE);
-//  TV.select_font(font4x6);
-//  TV.print(0,0,i);
-//  delay(50);
-//  TV.clear_screen();
-//  }
-//  for(int j = TV.hres()-4; j > 2; j-=2){
-//  TV.draw_circle(j, TV.vres()/2, 2, WHITE,WHITE);
-//  TV.print(0,0,j);
-//  delay(50);
-//  TV.clear_screen();
-//  }
-  TV.draw_rect(0, 6, 122, 88, WHITE);
-
-  x = analogRead(A0);
   TV.select_font(font4x6);
-  // TV.print(8,0,x);
-  x = map(x, 0, 1023, 20, 80);
+  TV.draw_rect(0, 6, 122, 88, WHITE);
+ 
+  TV.draw_circle(circle_x, circle_y, 2, WHITE, WHITE);
 
-  // TV.print(8,6,x);
+  // Desenha placar
   TV.print(8,0, p1_pontuacao);
+  TV.print(108,0, p2_pontuacao);
+
+  // Desenha a posição do jogador 1
+  x = analogRead(A0);
+  x = map(x, 0, 1023, 20, 80);
   p1_cima = x+12;
   p1_baixo = x-12;
   TV.draw_line(5, p1_baixo, 5, p1_cima, WHITE);
-  // TV.set_pixel(x, TV.vres()/2 - 10, WHITE);
-  // TV.set_pixel(5, x, WHITE);
 
+  // Desenha a posição do jogador 2
   x = analogRead(A1);
-  TV.select_font(font4x6);
-  // TV.println(108,0,x);
-  // x = map(x, 0, 1023, 12, 110);
   x = map(x, 0, 1023, 20, 80);
-  // TV.println(108,6,x);
-  //TV.println(108,0,x);
-  TV.print(108,0, p2_pontuacao);
-  // TV.set_pixel(x, TV.vres()/2 + 10, WHITE);
-  // TV.set_pixel(117, x, WHITE);
   p2_cima = x+12;
   p2_baixo = x-12;
   TV.draw_line(117, p2_baixo, 117, p2_cima, WHITE);
@@ -140,94 +210,14 @@ void jogo(){
   //TV.print("x");
   //x = TV.vres();
   //TV.print(x);
- // delay(15);
+  // delay(15);
 
   circle_x = circle_x + dir_x*2;
   circle_y = circle_y + dir_y;
 
-  //colisão da bolinha
-  //batendo no P1 ou na quina
-  if(circle_x == 8 || circle_x == 7){
-  //bateu no meio
-  if(circle_y <= p1_cima-6 && circle_y >= p1_baixo+6){
-    dir_y = 0;
-    dir_x *= -1;
-  }
-  //bateu em cima
-  else if(circle_y <= p1_cima && circle_y >= p1_cima-5){
-    dir_y= 1;
-    dir_x *= -1;
-  }
-  //bateu em baixo
-  else if(circle_y >= p1_baixo && circle_y <= p1_baixo+5){
-    dir_y= -1;
-    dir_x *= -1;
-  }
-  }
-  //batendo no P2 ou na
-  if(circle_x == 115 || circle_x ==114){
-  //bateu no meio
-  if(circle_y <= p2_cima-6 && circle_y >= p2_baixo+6){
-    dir_y = 0;
-    dir_x *= -1;
-  }
-  //bateu em cima
-  else if(circle_y <= p2_cima && circle_y >= p2_cima-5){
-    dir_y= 1;
-    dir_x *= -1;
-  }
-  //bateu em baixo
-  else if(circle_y >= p2_baixo && circle_y <= p2_baixo+5){
-    dir_y= -1;
-    dir_x *= -1;
-  }
-  }
-
-  //colisão em cima
-  if(circle_y == 8){
-  dir_y *= -1;
-  }
- 
-  //colisão em baixo
-  if(circle_y == 91){
-  dir_y *= -1;
-  }
-
-  //Bateu na direita
-  if(circle_x >= 120){
-  dir_x *= -1;
-  dir_y= 0;
-  p1_pontuacao += 1; //Aumenta pontuação do Player 1
-
-  if(p1_pontuacao == 5){
-
-    estado = VITORIA;
-
-    jogador_vitorioso = 1;
-  }
-
-  //Inicializa a posição da bola
-  circle_x = TV.hres()/2;
-  circle_y = TV.vres()/2;
-  }
-  //Bateu na esquerda
-  else if(circle_x <= 2){
-  dir_x *= -1;
-  dir_y= 0;
-  p2_pontuacao += 1; //Aumenta pontuação do Player 2
-
-  if(p2_pontuacao == 5){
-
-    estado = VITORIA;
-
-    jogador_vitorioso = 2;
-  }
-
-  //Inicializa a posição da bola
-  circle_x = TV.hres()/2;
-  circle_y = TV.vres()/2;
-  }
-
+  colisao_jogador(circle_x, circle_y);
+  colisao_campo(circle_x, circle_y);
+  colisao_pontuacao(circle_x, circle_y);
 }
 
 void vitoria(){  
@@ -257,3 +247,5 @@ void vitoria(){
     }
     last_state = leitura;
 }
+
+
